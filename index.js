c68cdb8ee7aa2a858f7b29b511d4229783cae883
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-07-07 19:59:34 +0400

    client

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const google_oauth = require("passport-google-oauth20").Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({
-        bye: 'buddy'
-    });
-});
+passport.use(new google_oauth());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 //node index.js
 //localhost:5000
+//https://console.developers.google.com
