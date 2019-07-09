9fcf15204083da2d25ee034798283076c8ef9fde
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-07-09 14:41:07 +0400

    login-logout

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookie_session = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+require('./models/user'); //in this order
 require('./services/passport');
-require('./models/user');
 
 mongoose.connect(keys.mongoUri);
 const app = express();
+
+app.use(
+    cookie_session({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.coockieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/auth_routes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require('passport');
 const google_oauth = require("passport-google-oauth20").Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+const user = mongoose.model('users');
+
+passport.serializeUser((User, done) => {
+    done(null, User.id);
+});
+
+passport.deserializeUser((id, done) => {
+    user.findById(id).then(User => {
+        done(null, User);
+    });
+});
 
 passport.use(
     new google_oauth({
@@ -9,8 +21,15 @@ passport.use(
         callbackURL: '/auth/google/callback'
     }, 
     (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
+        user.findOne({googleID: profile.id})
+            .then((existingUser) => {
+                if(existingUser) {
+                    done(null, existingUser);
+                }
+                else {
+                    new user ({googleID: profile.id}).save()
+                        .then(user => done(null, user));
+                }
+            });
     }
 ));
