fc21e4452dd0da3d527f6eeb7f14ca3061f088eb
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-08-19 19:44:26 +0400

    adding routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const Header = () => <h2>T</h2>
+const Dashboard = () => <h2>I</h2>
+const SurveyNew = () => <h2>M</h2>
+const Landing = () => <h2>A</h2>
 
 const App = () => {
     return(
         <div>
-            Salam voram!
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route exact path="/" component = {Landing}/>
+                    <Route exact path="/surveys" component = {Dashboard}/>
+                    <Route exact path="/surveys/new" component = {SurveyNew}/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
