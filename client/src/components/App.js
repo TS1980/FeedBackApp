c608d7ba30899b527a600d535438c5c675dd8248
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-08-24 14:33:49 +0400

    'materializecss'

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-const Header = () => <h2>T</h2>
+import Header from './header'
 const Dashboard = () => <h2>I</h2>
 const SurveyNew = () => <h2>M</h2>
 const Landing = () => <h2>A</h2>
