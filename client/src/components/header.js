b611f6c88dcce763d8ee912c273e736eae21ca8b
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-08-25 14:47:32 +0400

    color

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,7 +4,7 @@ class Header extends Component{
     render() {
         return(
             <nav>
-                <div className="nav-wrapper">
+                <div className="nav-wrapper blue darken-4">
                     <a className="left brand-logo">
                         FeedBackApp
                     </a>
