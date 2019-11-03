5b571753fcb7c973e68aa8d5657cb21b79dfd88d
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-11-03 17:16:10 +0400

    com

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import {FETCH_USER} from '../actions/types'
 
 //returns null or payload(logged/user model) or false (not logged)
-export default function(state = null, action) {
+export default function(state = {}, action) {
     console.log(action);
     switch(action.type){
         case FETCH_USER:
