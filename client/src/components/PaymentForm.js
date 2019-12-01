f699a9e33f46e695edf45e71948d05d507326c01
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-12-01 14:40:52 +0400

    dis edits

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -5,11 +5,17 @@ class PaymentForm extends Component {
     render() {
         return(
             <StripeCheckout
+                name="Payment"
+                description="Buy credits to create a survey"
                 amount = {500}
                 //callback func after auth stripe API
                 token = {token => console.log(token)}
                 stripeKey = {process.env.REACT_APP_STRIPE_KEY}
-            />
+            >
+                <button className="btn blue darken-4" color="white">
+                    Add credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -14,8 +14,8 @@ class Header extends Component {
                 );
             default:
                 return [
-                    <li><PaymentForm /></li>,
-                    <li><a href="/api/logout">Logout</a></li>
+                    <li key="1"><PaymentForm /></li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
                 ];
         }
     }
