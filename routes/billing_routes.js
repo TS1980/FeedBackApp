d33f4f95cbb31eb29635b26c6944bc3bace89d53
TS1980 <31511030+TS1980@users.noreply.github.com>
2019-12-02 14:22:09 +0400

    dummy

--- routes/billing_routes.js
+++ routes/billing_routes.js
@@ -2,5 +2,7 @@ const keys = require('../config/keys');
 const stripe =  require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-    app.post('/app/stripe', (req, res) => {});
+    app.post('/app/stripe', (req, res) => {
+        console.log(req.body);
+    });
 };
