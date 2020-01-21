80e02aabce612c17f2487fa0ee3d3786222ddf8e
TS1980 <31511030+TS1980@users.noreply.github.com>
2020-01-21 22:46:31 +0400

    start with prod

--- index.js
+++ index.js
@@ -23,5 +23,17 @@ app.use(passport.session());
 require('./routes/auth_routes')(app);
 require('./routes/billing_routes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //prodution assets как main.js
+    //найти файл, подходящий к запросу
+    app.use(express.static('client/build'));
+    //index.html, если не найдёт
+    //если кто-то делает не известный нам запрос, просто фигачь html по дефолту
+    const path = require('path');
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
