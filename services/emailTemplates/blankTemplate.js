7d2b149afd4d4c09c524ebdcfa9c4da9ec9e30e2
TS1980 <31511030+TS1980@users.noreply.github.com>
2020-02-06 00:23:54 +0400

    customize template 1

--- services/emailTemplates/blankTemplate.js
+++ services/emailTemplates/blankTemplate.js
@@ -1,3 +1,19 @@
 module.exports = (blank) => {
-    return '<div>' + blank.body + '</div>';
+    return `
+        <html>
+            <body>
+                <div style="text-align: center;">
+                    <h3>Feedback check!</h3>
+                    <p>We need your vote!</p>
+                    <p>${blank.body}</p>
+                    <div>
+                        <a href = "http://localhost:3000/">Yes</a>
+                    </div>
+                    <div>
+                        <a href = "http://localhost:3000/">No</a>
+                    </div>
+                </div>
+            </body>
+        </html>
+    `;
 };
